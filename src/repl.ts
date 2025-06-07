cdddb9d246d78f89908370703b5d1231cea1bb76
HamzaAkb <hamzawakbar@gmail.com>
2025-06-08 00:45:18 +0500

    feat(map): integrate PokeAPI location-area commands with pagination

--- src/repl.ts
+++ src/repl.ts
@@ -1,10 +1,6 @@
 import { State } from "./state.js";
 
-export function cleanInput(input: string): string[] {
-  return input.trim().split(/\s+/).filter(Boolean);
-}
-
-export function startREPL(state: State) {
+export async function startREPL(state: State) {
 	state.readline.prompt();
 
 	state.readline.on("line", async (input) => {
@@ -26,11 +22,19 @@ export function startREPL(state: State) {
 		}
 
 		try {
-			cmd.callback(state);
+			await cmd.callback(state);
 		} catch (e) {
-			console.log(e);
+			console.log((e as Error).message);
 		}
 
 		state.readline.prompt();
 	});
+}
+
+export function cleanInput(input: string): string[] {
+	return input
+		.toLowerCase()
+		.trim()
+		.split(" ")
+		.filter((word) => word !== "");
 }
